f72d01e922a01b193f9e94aed952c1395e963a02
udothemath1984 <udothemath1984@Pros-MacBook-Pro.local>
2015-12-21 02:52:22 -0500

    save lattice parameter

--- amcsd_list_for_cif.py
+++ amcsd_list_for_cif.py
@@ -12,8 +12,9 @@ class CifInfo:
     #
     # just for convenience
     def __init__(self):
-        self.two_theta = []
-        self.intensity = []
+        self.cellparam = [None] * 10
+        self.spacegroup = ''
+
 
 def get_list():
     # https://pypi.python.org/pypi/scraperwiki
@@ -55,9 +56,8 @@ def get_materials_count_and_indices_with_cif(name=''):
     for href in hrefs:  
         if '/AMS/download.php?id=' and '.cif&down=cif' in href:
             data_count_and_indices = data_count_and_indices + p.findall(href)
-    #print all
     data_count_and_indices.insert(0, len(data_count_and_indices))
-    #print data_count_and_indices
+    #print data_count_and_indices, [number, first_evidence, second_evidence, ...]
     return data_count_and_indices
 
 def get_into_cif_file(material_index=''): 
@@ -65,11 +65,39 @@ def get_into_cif_file(material_index=''):
     cif_info = urllib2.urlopen('http://rruff.geo.arizona.edu/AMS/CIF_text_files/{0}_cif.txt'.format(material_index)) 
     # Remove \n  
     #http://stackoverflow.com/questions/12330522/reading-a-file-without-newlines
-    access_cif = cif_info.read().splitlines()
-    
+    access_cif = cif_info.read().splitlines()   
     # Remain \n    
     #access_cif = cif_info.readlines()  
     return access_cif
+
+
+def get_info_from_cif(material_index=''): 
+    import urllib2
+    cif_info = urllib2.urlopen('http://rruff.geo.arizona.edu/AMS/CIF_text_files/{0}_cif.txt'.format(material_index)) 
+    # Remove \n  
+    #http://stackoverflow.com/questions/12330522/reading-a-file-without-newlines
+    access_cif = cif_info.read().splitlines()
+    cif_info = CifInfo()    
+    
+    for item in access_cif[:40]: 
+        if '_cell_length_a' in item:
+            cif_info.cellparam[0] = item.split()[1]
+        if '_cell_length_b' in item:
+            cif_info.cellparam[1] = item.split()[1]
+        if '_cell_length_c' in item:
+            cif_info.cellparam[2] = item.split()[1]
+        if '_cell_angle_alpha' in item:
+            cif_info.cellparam[3] = item.split()[1]
+        if '_cell_angle_beta' in item:
+            cif_info.cellparam[4] = item.split()[1]
+        if '_cell_angle_gamma' in item:
+            cif_info.cellparam[5] = item.split()[1]
+        if '_cell_volume' in item:
+            cif_info.cellparam[6] = item.split()[1]            
+        if '_exptl_crystal_density_diffrn' in item:
+            cif_info.cellparam[7] = item.split()[1]            
+                        
+    return cif_info
     
  
 def pattern_finder(read_list, start='',end=''):
--- main_get_cif.py
+++ main_get_cif.py
@@ -7,27 +7,37 @@ Created on Sun Dec 20 12:38:55 2015
 import amcsd_list_for_cif
 import re
 
+# Entire list
 name_list = amcsd_list_for_cif.get_list()
 print name_list 
 
 dic_book = {}
 
 for name in name_list:
-    #if any(x in name for x in typo_list):
-    #    continue
+
+    # cif name of the materials 
     print name
     count_indices = amcsd_list_for_cif.get_materials_count_and_indices_with_cif(name)
     print count_indices[1] # Show first dataset
      
-    current_cif = amcsd_list_for_cif.get_into_cif_file(count_indices[1])[:8] 
-    #print current_cif
-
-    # http://stackoverflow.com/questions/7421621/python-read-through-file-until-match-read-until-next-pattern
-    block_top='data_global'   
-    block_bot='_journal_name_full'   
-    print current_cif
-    # Search for the block contains certain pattern       
-    print amcsd_list_for_cif.pattern_finder(current_cif, block_top, block_bot)
+#==============================================================================
+#     # cif file index 
+#     current_cif = amcsd_list_for_cif.get_into_cif_file(count_indices[1])[:8] 
+#     print current_cif
+#==============================================================================
+    
+    
+    info = amcsd_list_for_cif.get_info_from_cif(count_indices[1])    
+    print info.cellparam
+    
+#==============================================================================
+# 
+#     # Search for the block contains certain pattern       
+#     # http://stackoverflow.com/questions/7421621/python-read-through-file-until-match-read-until-next-pattern
+#     block_top='data_global'   
+#     block_bot='_journal_name_full'   
+#     print amcsd_list_for_cif.pattern_finder(current_cif, block_top, block_bot)
+#==============================================================================
  
     # Save the info into dictionary
     dic_book[name] = count_indices     
